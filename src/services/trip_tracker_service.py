7edbcc0ffcd135bca40ad531f3c4efb86d8a52a3
Vili Sinervä <vili.sinerva@helsinki.fi>
2022-04-01 14:52:52 +0300

    Add main service class

---
+++ src/services/trip_tracker_service.py
@@ -0,0 +1,16 @@
+from repositories.profile_repository import profile_repository
+
+class TripTrackerService:
+    def __init__(self):
+        pass
+
+    def get_profiles(self):
+        return profile_repository.list_all()
+
+    def add_profile(self, name : str):
+        profile_repository.add(name)
+
+    def remove_profile(self, name : str):
+        profile_repository.remove(name)
+
+trip_tracker_service = TripTrackerService()
