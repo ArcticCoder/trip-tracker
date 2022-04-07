f2d46721535e7f21908aa3966d4e66bf3e9e6bed
Vili Sinervä <vili.sinerva@helsinki.fi>
2022-04-07 13:03:24 +0300

    Add pylint and 'lint' invoke task to project"

--- tasks.py
+++ tasks.py
@@ -10,6 +10,10 @@ def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
 
 @task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
+
+@task
 def test(ctx):
     ctx.run("pytest src", pty=True)
 
