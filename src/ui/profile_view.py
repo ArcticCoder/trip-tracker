803c04d91fc21b2c60c20791d0114dd4dc1dab65
Vili Sinervä <vili.sinerva@helsinki.fi>
2022-04-07 20:07:15 +0300

    Change occurrences of 'user' to 'profile' for consistency

--- src/ui/profile_view.py
+++ src/ui/profile_view.py
@@ -8,21 +8,21 @@ class ProfileView():
         self._root = root
         self._handle_profile_select = handle_profile_select
         self._frame = tk.Frame(self._root)
-        self._user_frame = tk.Frame(self._frame)
+        self._profile_frame = tk.Frame(self._frame)
 
-        self._new_user_frame = tk.Frame(self._frame, pady=5)
+        self._new_profile_frame = tk.Frame(self._frame, pady=5)
         self._new_name_lbl = tk.Label(
-            self._new_user_frame, text="Nimi:", padx=5)
-        self._new_name_entry = tk.Entry(self._new_user_frame)
+            self._new_profile_frame, text="Nimi:", padx=5)
+        self._new_name_entry = tk.Entry(self._new_profile_frame)
         self._new_name_entry.bind(
             "<Return>", lambda event: self._handle_add_btn())
-        self._new_user_btn = tk.Button(
-            self._new_user_frame, text="Lisää", command=self._handle_add_btn, padx=5)
+        self._new_profile_btn = tk.Button(
+            self._new_profile_frame, text="Lisää", command=self._handle_add_btn, padx=5)
 
         self._new_name_lbl.grid(row=0, column=0)
         self._new_name_entry.grid(row=0, column=1)
-        self._new_user_btn.grid(row=0, column=2)
-        self._new_user_frame.pack(fill=tk.constants.BOTH, expand=True)
+        self._new_profile_btn.grid(row=0, column=2)
+        self._new_profile_frame.pack(fill=tk.constants.BOTH, expand=True)
 
         self._print_profiles()
 
@@ -36,26 +36,26 @@ class ProfileView():
         self._root.resizable(True, True)
 
     def _print_profiles(self):
-        if self._user_frame:
-            self._user_frame.destroy()
+        if self._profile_frame:
+            self._profile_frame.destroy()
 
-        users = trip_tracker_service.get_profiles()
-        if users:
-            self._user_frame = tk.Frame(self._frame)
+        profiles = trip_tracker_service.get_profiles()
+        if profiles:
+            self._profile_frame = tk.Frame(self._frame)
 
-            for i in range(len(users)):
-                text = users[i][1]
-                name_btn = tk.Button(self._user_frame, text=text, anchor=tk.constants.W,
+            for i in range(len(profiles)):
+                profile_id, name = profiles[i]
+                name_btn = tk.Button(self._profile_frame, text=name, anchor=tk.constants.W,
                                      activebackground="grey", bg="lightgrey", highlightbackground="black", relief=tk.constants.FLAT,
-                                     command=partial(self._handle_profile_select, users[i][1]))
+                                     command=partial(self._handle_profile_select, profile_id))
                 name_btn.grid(row=i, column=0, sticky=tk.constants.EW)
 
-                del_btn = tk.Button(self._user_frame, text="X", anchor=tk.constants.W,
+                del_btn = tk.Button(self._profile_frame, text="X", anchor=tk.constants.W,
                                     activebackground="grey", bg="lightgrey", highlightbackground="black", relief=tk.constants.FLAT,
-                                    command=partial(self._del_btn_click, users[i][0]))
+                                    command=partial(self._del_btn_click, profile_id))
                 del_btn.grid(row=i, column=1, sticky=tk.constants.EW)
 
-            self._user_frame.pack(fill=tk.constants.BOTH, expand=True)
+            self._profile_frame.pack(fill=tk.constants.BOTH, expand=True)
 
     def _handle_add_btn(self):
         name = self._new_name_entry.get()
