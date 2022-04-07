a7a35e0aafc1d8e1accac12e7ed8e2ee8c075d51
Vili Sinervä <vili.sinerva@helsinki.fi>
2022-04-07 15:23:01 +0300

    Add trips table to db

--- src/db_management.py
+++ src/db_management.py
@@ -3,11 +3,17 @@ from db_connection import get_db_connection
 
 def drop_tables(connection):
     connection.execute("DROP TABLE IF EXISTS profiles;")
+    connection.execute("DROP TABLE IF EXISTS trips;")
 
 
 def create_tables(connection):
     connection.execute("CREATE TABLE IF NOT EXISTS profiles"
                        "(id INTEGER PRIMARY KEY, name TEXT UNIQUE);")
+    connection.execute("CREATE TABLE IF NOT EXISTS trips"
+                       "(id INTEGER PRIMARY KEY, profile_id INTEGER NOT NULL,"
+                       "name TEXT NOT NULL, start_time TEXT, end_time TEXT,"
+                       "length REAL,"
+                       "FOREIGN KEY(profile_id) REFERENCES profiles(id));")
 
 
 def init_db():
